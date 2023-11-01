131f01bd6177b223591062031e2fc039e82c4a93
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2023-11-01 12:25:00 -0700

    hw4

---
+++ hw3/double.sh
@@ -0,0 +1,3 @@
+python cs285/scripts/run_hw3_dqn.py -cfg experiments/dqn/lunarlander_doubleq.yaml --seed 1
+python cs285/scripts/run_hw3_dqn.py -cfg experiments/dqn/lunarlander_doubleq.yaml --seed 3
+
---
+++ hw4/q4.sh
@@ -0,0 +1,7 @@
+#python cs285/scripts/run_hw4.py -cfg experiments/mpc/problem4/reacher_ablation_cas100.yaml
+#python cs285/scripts/run_hw4.py -cfg experiments/mpc/problem4/reacher_ablation_cas5000.yaml
+#python cs285/scripts/run_hw4.py -cfg experiments/mpc/problem4/reacher_ablation_es1.yaml
+python cs285/scripts/run_hw4.py -cfg experiments/mpc/problem4/reacher_ablation_es10.yaml
+python cs285/scripts/run_hw4.py -cfg experiments/mpc/problem4/reacher_ablation_ph3.yaml
+python cs285/scripts/run_hw4.py -cfg experiments/mpc/problem4/reacher_ablation_ph20.yaml
+python cs285/scripts/run_hw4.py -cfg experiments/mpc/problem4/reacher_ablation.yaml
